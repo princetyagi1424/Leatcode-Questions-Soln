18f88af617fda0079ab9910a56b61cab64b4d2e8
PRINCE TYAGI <princetyagi3726@gmail.com>
2026-08-24 19:43:17 +0530

    Time: 6 ms (52.23%), Space: 45.4 MB (77.8%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0,y=0;
+        for(int i=0;i<moves.length();i++){
+            char ch=moves.charAt(i);
+            if(ch=='U'){
+                x++;
+            }else if(ch=='D'){
+                x--;
+            }else if(ch=='L'){
+                y++;
+            }else if(ch=='R'){
+                y--;
+            }
+        }
+        return x==0 && y==0;
+    }
+}

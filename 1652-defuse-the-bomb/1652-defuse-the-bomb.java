7f363c327fbe35740a3d2f461b6b85e28e231293
PRINCE TYAGI <princetyagi3726@gmail.com>
2026-08-24 20:04:41 +0530

    Time: 2 ms (60.49%), Space: 44.1 MB (13.87%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n=code.length;
+        int[] ans=new int[n];
+        for (int i=0; i<n; i++) {
+            if (k > 0) {
+                for (int j=1;j<=k;j++) {
+                    ans[i]+= code[(i +j) % n];
+                }
+            } else if (k< 0) {
+                for (int j= 1; j<= -k;j++) {
+                    ans[i]+=code[(i -j +n) % n];
+                }
+            }
+        }
+        return ans;
+    }
+}

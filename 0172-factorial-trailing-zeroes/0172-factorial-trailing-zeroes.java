83603df93d13b37277d6dfa212b81676e11c818f
PRINCE TYAGI <princetyagi3726@gmail.com>
2026-09-05 21:16:34 +0530

    Time: 0 ms (100%), Space: 42.5 MB (17.07%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int ans=0;
+        while(n>=5){
+            n=n/5;
+            ans+=n;
+        }
+        return ans;
+    }
+}

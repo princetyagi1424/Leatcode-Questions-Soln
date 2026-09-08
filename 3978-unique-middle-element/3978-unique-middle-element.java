6542e99e2653cf283ea983e428dc2111c268390a
PRINCE TYAGI <princetyagi3726@gmail.com>
2026-09-08 20:41:11 +0530

    Time: 1 ms (93.57%), Space: 45.9 MB (98.12%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.java
+++ 3978-unique-middle-element/3978-unique-middle-element.java
@@ -1,16 +1,10 @@
 class Solution {
     public boolean isMiddleElementUnique(int[] nums) {
-        boolean ans=true;
-        int mid=(nums.length/2);
-        int l=0,r=nums.length-1;
-        while(l<mid){
-            if(nums[l]==nums[mid] || nums[r]==nums[mid]){
-                ans=false;
-                break;
-            }
-            l++;
-            r--;
+        int n = nums.length;
+        int k = nums[n / 2];
+        for(int i = 0 ; i < n / 2 ; i++){
+            if(nums[i] == k || nums[i + (n / 2) + 1] == k) return false;
         }
-        return ans;
+        return true;
     }
 }

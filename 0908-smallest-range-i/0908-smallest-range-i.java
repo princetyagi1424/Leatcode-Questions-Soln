da332802efc3f16887dcc48c753c2c36d89a6bef
PRINCE TYAGI <princetyagi3726@gmail.com>
2026-09-20 18:19:06 +0530

    Time: 3 ms (80.11%), Space: 47.4 MB (21.12%) - LeetHub

---
+++ 0908-smallest-range-i/0908-smallest-range-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int smallestRangeI(int[] nums, int k) {
+        int max=nums[0];
+        int min=nums[0];
+        for(int num:nums){
+            max=Math.max(max,num);
+            min=Math.min(min,num);
+        }
+        return Math.max(0,max-min-2*k);
+    }
+}

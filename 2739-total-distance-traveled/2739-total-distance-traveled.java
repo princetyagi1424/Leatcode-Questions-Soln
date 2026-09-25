de232887fe40a1240d052a3bc890ee1a95defbaf
PRINCE TYAGI <princetyagi3726@gmail.com>
2026-09-25 21:50:59 +0530

    Time: 4 ms (100%), Space: 46.1 MB (14.07%) - LeetHub

---
+++ 2739-total-distance-traveled/2739-total-distance-traveled.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int distanceTraveled(int mainTank, int additionalTank) {
+        int ans=0;
+        while(mainTank>=5){
+            ans+=50;
+            mainTank-=5;
+            if(additionalTank>0){
+                additionalTank--;
+                mainTank++;
+            }
+        }
+        return ans+mainTank*10;
+    }
+}
